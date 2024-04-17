c4c3eeef061242257763192f5340c01aba418de1
Alusonn <alanlossendiere@gmail.com>
2024-04-17 00:32:45 -0300

    Listo portfolio, se eliminino error de vercel

--- app/ui/Contact.jsx
+++ app/ui/Contact.jsx
@@ -1,6 +1,3 @@
-import { CiMail } from "react-icons/ci";
-import { CiPhone } from "react-icons/ci";
-
 export const Contact = () => {
   return (
     <div className="w-full bg-gray py-16 md:py-20 2xl:py-24" id="Contacto">
@@ -12,20 +9,18 @@ export const Contact = () => {
             </div>
           </div>
           <p className="text-lg md:text-xl text-center max-w-xl">
-            En caso de querer contactarme para algun trabajo o proyecto.
-            Estoy disponible 😎
+            En caso de querer contactarme para algun trabajo o proyecto. Estoy
+            disponible 😎
           </p>
         </div>
         <div className="flex flex-col items-center gap-6 md:gap-12">
           <div className="flex flex-col items-center md:gap-4">
             <div className="flex items-center gap-4 md:gap-5">
-              <CiMail size={40} />
               <h2 className="text-lg text-gray-900 font-semibold md:text-4xl tracking-[-0.02em]">
                 alanmxzone@hotmail.com
               </h2>
             </div>
             <div className="flex items-center gap-4 md:gap-5">
-              <CiPhone size={40} />
               <h2 className="text-lg text-gray-900 font-semibold md:text-4xl tracking-[-0.02em]">
                 +54 2255 416825
               </h2>
